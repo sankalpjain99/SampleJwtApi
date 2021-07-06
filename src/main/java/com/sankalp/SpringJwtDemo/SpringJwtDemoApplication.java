cffbde97f6fa51b2768747807e18fb33bb836cd8
Sankalp <sankalpjain99@gmail.com>
2021-07-06 14:55:07 +0530

    Added Swagger for Rest API

--- src/main/java/com/sankalp/SpringJwtDemo/SpringJwtDemoApplication.java
+++ src/main/java/com/sankalp/SpringJwtDemo/SpringJwtDemoApplication.java
@@ -2,12 +2,27 @@ package com.sankalp.SpringJwtDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class SpringJwtDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJwtDemoApplication.class, args);
+	}
+	
+	@Bean
+	public Docket swaggerConfig() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.sankalp"))
+				.build();
 	}
 
 }
